be771ab2ecfe4d309a17ca4373fc37ecf0f6b0d7
Ultiminator <UltimateAlienForce@Outlook.com>
2024-06-26 04:08:36 +0300

    now when the user cancels an order, the ordered items amount is returned to the stock

--- Accounts/Orders/cancel.php
+++ Accounts/Orders/cancel.php
@@ -7,10 +7,28 @@ $dbConn = mysqli_connect($mysqlHost, $mysqlUser, $mysqlPassword, $dbName);
 if (mysqli_connect_errno()){
     die("error while connecting to database");
 }
-//check status of the order
+//change the order status to cancelled
 $query = "update orders set stat = 5 where id = '$orderId'";
 if (!mysqli_query($dbConn, $query)) {
     die("couldn't cancel order");
+}
+//now lets try to return the items to the stock 
+//first lets get items info and its quantity
+$items = [];
+$query = "select itemId, quantity from orderedItems where orderId = '$orderId'";
+$result = mysqli_query($dbConn, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+    $items[] = $row;
+}
+//well we have the items and the ordered quantity of each item
+//now let's add this amount to the stock of each item
+foreach ($items as $item) {
+    $query = "select amount from items where id = " . $item['itemId'];
+    $result = mysqli_query($dbConn, $query);
+    $row = mysqli_fetch_assoc($result);
+    $newAmount = $row['amount'] + $item['quantity'];
+    $query = "update items set amount = '$newAmount' where id = " . $item['itemId'];
+    mysqli_query($dbConn, $query);
 }
 echo "success";
 ?>
